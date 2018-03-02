ac01aea5b67a465033dbacb1496dddf7ac11702e
AdrienDuot <35662723+AdrienDuot@users.noreply.github.com>
2018-03-02 17:45:24 +0100

    mob

---
+++ test multi/Assets/camerascript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerascript : MonoBehaviour {
+
+	// Use this for initialization
+
+	public Transform player;
+	public Transform basgauche;
+	public Transform basdroite;
+	public Transform hautgauche;
+	public Transform hautdroite;
+	
+	
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (player.transform.position.x < basdroite.position.x && player.transform.position.x > basgauche.position.x && player.transform.position.z<basdroite.position.z)
+		{
+			
+		}
+	}
+}
